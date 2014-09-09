cee56922cd03dacd59b03022197ebf98a9985de3
jhaap <joose.haapaniemi@metropolia.fi>
2014-09-09 20:00:54 +0300

    moment

--- test.js
+++ test.js
@@ -1,15 +1,19 @@
 var m = require('./last-mod');
-var moment = require('moment');
+
 var filename = process.argv[2];
 
 var timestamp = m(filename);
 if (timestamp !== false) {
 	var date = new Date(timestamp);
 
-	var fin = moment(date).format('D[.]M[.]YYYY[ ]H[:]mm');
-	
-	
-	console.log(fin);
+	var fin = date.getDate() + '.' +
+		(date.getMonth() + 1) + '.' +
+		date.getFullYear();
+		
+	fin += ' ' + date.getHours() + '.' +
+		date.getMinutes();
+		
+		console.log(fin);
 }
 else {
 	console.log("Error!");
